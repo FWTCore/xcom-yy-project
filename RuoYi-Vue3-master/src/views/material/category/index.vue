7b26a4de46928ad0c50bc839ac4d85043f14c90f
xudongxu <xudongxu@yzwec.corp>
2025-10-03 22:50:37 +0800

    样式条件

--- RuoYi-Vue3-master/src/views/material/category/index.vue
+++ RuoYi-Vue3-master/src/views/material/category/index.vue
@@ -10,13 +10,13 @@
           <el-option key="0" label="否" value="0"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="物资名称必填" prop="hasMaterialName" style="width: 100px">
+      <el-form-item label="物资名称必填" prop="hasMaterialName" style="width: 340px">
         <el-select v-model="queryParams.hasMaterialName" placeholder="请选择" style="width: 240px" clearable>
           <el-option key="1" label="是" value="1"></el-option>
           <el-option key="0" label="否" value="0"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="规格型号必填" prop="hasSpecification" style="width: 100px">
+      <el-form-item label="规格型号必填" prop="hasSpecification" style="width: 340px">
         <el-select v-model="queryParams.hasSpecification" placeholder="请选择" style="width: 240px" clearable>
           <el-option key="1" label="是" value="1"></el-option>
           <el-option key="0" label="否" value="0"></el-option>
