4d0057cbcb46a1a59898c582f62edaad854beacb
xudongxu <xudongxu@yzwec.corp>
2025-10-05 11:29:15 +0800

    code

--- RuoYi-Vue3-master/src/views/business/employee/index.vue
+++ RuoYi-Vue3-master/src/views/business/employee/index.vue
@@ -1,77 +1,13 @@
 <template>
   <div class="app-container">
     <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
-      <el-form-item label="系统公司id" prop="deptId">
-        <el-input
-          v-model="queryParams.deptId"
-          placeholder="请输入系统公司id"
-          clearable
-          @keyup.enter="handleQuery"
-        />
+      <el-form-item label="归属公司" prop="deptId">
+        <el-tree-select v-model="queryParams.deptId" :data="enabledDeptOptions"
+          :props="{ value: 'id', label: 'label', children: 'children' }" value-key="id" placeholder="请选择归属公司"
+          style="width: 240px" clearable />
       </el-form-item>
       <el-form-item label="员工名称" prop="employeeName">
-        <el-input
-          v-model="queryParams.employeeName"
-          placeholder="请输入员工名称"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="是否删除" prop="deleteFlag">
-        <el-input
-          v-model="queryParams.deleteFlag"
-          placeholder="请输入是否删除"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="创建人id" prop="createdById">
-        <el-input
-          v-model="queryParams.createdById"
-          placeholder="请输入创建人id"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="创建人" prop="createdByName">
-        <el-input
-          v-model="queryParams.createdByName"
-          placeholder="请输入创建人"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="创建时间" prop="createdTime">
-        <el-date-picker clearable
-          v-model="queryParams.createdTime"
-          type="date"
-          value-format="YYYY-MM-DD"
-          placeholder="请选择创建时间">
-        </el-date-picker>
-      </el-form-item>
-      <el-form-item label="更新人id" prop="updatedById">
-        <el-input
-          v-model="queryParams.updatedById"
-          placeholder="请输入更新人id"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="更新人" prop="updatedByName">
-        <el-input
-          v-model="queryParams.updatedByName"
-          placeholder="请输入更新人"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="更新时间" prop="updatedTime">
-        <el-date-picker clearable
-          v-model="queryParams.updatedTime"
-          type="date"
-          value-format="YYYY-MM-DD"
-          placeholder="请选择更新时间">
-        </el-date-picker>
+        <el-input v-model="queryParams.employeeName" placeholder="请输入员工名称" clearable @keyup.enter="handleQuery" />
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
@@ -81,122 +17,68 @@
 
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
-        <el-button
-          type="primary"
-          plain
-          icon="Plus"
-          @click="handleAdd"
-          v-hasPermi="['business:employee:add']"
-        >新增</el-button>
+        <el-button type="primary" plain icon="Plus" @click="handleAdd"
+          v-hasPermi="['business:employee:add']">新增</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="Edit"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['business:employee:edit']"
-        >修改</el-button>
+        <el-button type="success" plain icon="Edit" :disabled="single" @click="handleUpdate"
+          v-hasPermi="['business:employee:edit']">修改</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="Delete"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['business:employee:remove']"
-        >删除</el-button>
+        <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete"
+          v-hasPermi="['business:employee:remove']">删除</el-button>
       </el-col>
-      <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="Download"
-          @click="handleExport"
-          v-hasPermi="['business:employee:export']"
-        >导出</el-button>
-      </el-col>
+      <!-- <el-col :span="1.5">
+        <el-button type="warning" plain icon="Download" @click="handleExport"
+          v-hasPermi="['business:employee:export']">导出</el-button>
+      </el-col> -->
       <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
     <el-table v-loading="loading" :data="employeeList" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="主键id;主键id" align="center" prop="id" />
-      <el-table-column label="系统公司id" align="center" prop="deptId" />
+      <el-table-column label="序号" align="center" width="50">
+        <template #default="scope">
+          <span>{{ calculateIndex(scope.$index) }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="公司名称" align="center" prop="deptName" />
       <el-table-column label="员工名称" align="center" prop="employeeName" />
-      <el-table-column label="是否删除" align="center" prop="deleteFlag" />
-      <el-table-column label="创建人id" align="center" prop="createdById" />
-      <el-table-column label="创建人" align="center" prop="createdByName" />
       <el-table-column label="创建时间" align="center" prop="createdTime" width="180">
         <template #default="scope">
           <span>{{ parseTime(scope.row.createdTime, '{y}-{m}-{d}') }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="更新人id" align="center" prop="updatedById" />
-      <el-table-column label="更新人" align="center" prop="updatedByName" />
-      <el-table-column label="更新时间" align="center" prop="updatedTime" width="180">
-        <template #default="scope">
-          <span>{{ parseTime(scope.row.updatedTime, '{y}-{m}-{d}') }}</span>
-        </template>
-      </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template #default="scope">
-          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['business:employee:edit']">修改</el-button>
-          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['business:employee:remove']">删除</el-button>
+          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
+            v-hasPermi="['business:employee:edit']">修改</el-button>
+          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
+            v-hasPermi="['business:employee:remove']">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
-    
-    <pagination
-      v-show="total>0"
-      :total="total"
-      v-model:page="queryParams.pageNum"
-      v-model:limit="queryParams.pageSize"
-      @pagination="getList"
-    />
+
+    <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum"
+      v-model:limit="queryParams.pageSize" @pagination="getList" />
 
     <!-- 添加或修改员工对话框 -->
-    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
+    <el-dialog :title="title" v-model="open" width="600px" append-to-body>
       <el-form ref="employeeRef" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="系统公司id" prop="deptId">
-          <el-input v-model="form.deptId" placeholder="请输入系统公司id" />
-        </el-form-item>
-        <el-form-item label="员工名称" prop="employeeName">
-          <el-input v-model="form.employeeName" placeholder="请输入员工名称" />
-        </el-form-item>
-        <el-form-item label="是否删除" prop="deleteFlag">
-          <el-input v-model="form.deleteFlag" placeholder="请输入是否删除" />
-        </el-form-item>
-        <el-form-item label="创建人id" prop="createdById">
-          <el-input v-model="form.createdById" placeholder="请输入创建人id" />
-        </el-form-item>
-        <el-form-item label="创建人" prop="createdByName">
-          <el-input v-model="form.createdByName" placeholder="请输入创建人" />
-        </el-form-item>
-        <el-form-item label="创建时间" prop="createdTime">
-          <el-date-picker clearable
-            v-model="form.createdTime"
-            type="date"
-            value-format="YYYY-MM-DD"
-            placeholder="请选择创建时间">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="更新人id" prop="updatedById">
-          <el-input v-model="form.updatedById" placeholder="请输入更新人id" />
-        </el-form-item>
-        <el-form-item label="更新人" prop="updatedByName">
-          <el-input v-model="form.updatedByName" placeholder="请输入更新人" />
-        </el-form-item>
-        <el-form-item label="更新时间" prop="updatedTime">
-          <el-date-picker clearable
-            v-model="form.updatedTime"
-            type="date"
-            value-format="YYYY-MM-DD"
-            placeholder="请选择更新时间">
-          </el-date-picker>
-        </el-form-item>
+        <el-row>
+          <el-col :span="12">
+            <el-form-item label="归属公司" prop="deptId">
+              <el-tree-select v-model="form.deptId" :data="enabledDeptOptions"
+                :props="{ value: 'id', label: 'label', children: 'children' }" value-key="id" placeholder="请选择归属公司"
+                clearable check-strictly />
+            </el-form-item>
+          </el-col>
+          <el-col :span="12">
+            <el-form-item label="员工名称" prop="employeeName">
+              <el-input v-model="form.employeeName" placeholder="请输入员工名称" />
+            </el-form-item>
+          </el-col>
+        </el-row>
       </el-form>
       <template #footer>
         <div class="dialog-footer">
@@ -210,6 +92,7 @@
 
 <script setup name="Employee">
 import { listEmployee, getEmployee, delEmployee, addEmployee, updateEmployee } from "@/api/business/employee"
+import { deptTreeSelect } from "@/api/system/user"
 
 const { proxy } = getCurrentInstance()
 
@@ -222,6 +105,7 @@ const single = ref(true)
 const multiple = ref(true)
 const total = ref(0)
 const title = ref("")
+const enabledDeptOptions = ref(undefined)
 
 const data = reactive({
   form: {},
@@ -229,14 +113,7 @@ const data = reactive({
     pageNum: 1,
     pageSize: 10,
     deptId: null,
-    employeeName: null,
-    deleteFlag: null,
-    createdById: null,
-    createdByName: null,
-    createdTime: null,
-    updatedById: null,
-    updatedByName: null,
-    updatedTime: null
+    employeeName: null
   },
   rules: {
     deleteFlag: [
@@ -341,12 +218,12 @@ function submitForm() {
 /** 删除按钮操作 */
 function handleDelete(row) {
   const _ids = row.id || ids.value
-  proxy.$modal.confirm('是否确认删除员工编号为"' + _ids + '"的数据项？').then(function() {
+  proxy.$modal.confirm('是否确认删除员工编号为"' + _ids + '"的数据项？').then(function () {
     return delEmployee(_ids)
   }).then(() => {
     getList()
     proxy.$modal.msgSuccess("删除成功")
-  }).catch(() => {})
+  }).catch(() => { })
 }
 
 /** 导出按钮操作 */
@@ -356,5 +233,34 @@ function handleExport() {
   }, `employee_${new Date().getTime()}.xlsx`)
 }
 
-getList()
+
+/** 查询公司下拉树结构 */
+function getDeptTree() {
+  deptTreeSelect().then(response => {
+    enabledDeptOptions.value = filterDisabledDept(JSON.parse(JSON.stringify(response.data)))
+  })
+}
+
+/** 过滤禁用的公司 */
+function filterDisabledDept(deptList) {
+  return deptList.filter(dept => {
+    if (dept.disabled) {
+      return false
+    }
+    if (dept.children && dept.children.length) {
+      dept.children = filterDisabledDept(dept.children)
+    }
+    return true
+  })
+}
+
+// 计算序号（考虑分页）
+const calculateIndex = (index) => {
+  return (queryParams.value.pageNum - 1) * queryParams.value.pageSize + index + 1
+}
+
+onMounted(() => {
+  getDeptTree()
+  getList()
+})
 </script>
