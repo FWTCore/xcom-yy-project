28b66309be9917af51aa67a5fc0054d33b76c3ca
xuchase <121086507@qq.com>
2025-11-28 23:24:25 +0800

    添加指标表

---
+++ RuoYi-Vue-master/ruoyi-business/src/main/java/com/ruoyi/business/domain/entity/MetricsDO.java
@@ -0,0 +1,35 @@
+package com.ruoyi.business.domain.entity;
+
+import com.ruoyi.common.core.domain.BaseEntityDO;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * 指标对象 metrics
+ *
+ * @author xcom
+ * @date 2025/11/28
+ */
+
+@Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class MetricsDO extends BaseEntityDO {
+
+    /** 业务id 数字 */
+    private Long   bizId;
+
+    /** 业务key 字符串 */
+    private String bizKey;
+
+    /** 业务类型 */
+    private Long   bizType;
+
+    /** 总数量 */
+    private Long   totalCount;
+
+    /** 核对数量 */
+    private Long   checkCount;
+
+}
---
+++ RuoYi-Vue-master/ruoyi-business/src/main/java/com/ruoyi/business/mapper/MetricsMapper.java
@@ -0,0 +1,70 @@
+package com.ruoyi.business.mapper;
+
+import com.ruoyi.business.domain.entity.MetricsDO;
+import com.ruoyi.business.domain.model.Metrics;
+
+import java.util.List;
+
+/**
+ * 指标Mapper接口
+ *
+ * @author xcom
+ * @date 2025-11-28
+ */
+public interface MetricsMapper {
+    /**
+     * 查询指标
+     *
+     * @param id 指标主键
+     * @return 指标
+     */
+    MetricsDO selectMetricsById(Long id);
+
+    /**
+     * 查询指标
+     *
+     * @param ids 指标主键
+     * @return 指标
+     */
+    List<MetricsDO> selectMetricsByIds(List<Long> ids);
+
+    /**
+     * 查询指标列表
+     *
+     * @param metrics 指标
+     * @return 指标集合
+     */
+    List<MetricsDO> selectMetricsList(Metrics metrics);
+
+    /**
+     * 新增指标
+     *
+     * @param metrics 指标
+     * @return 结果
+     */
+    int insertMetrics(MetricsDO metrics);
+
+    /**
+     * 修改指标
+     *
+     * @param metrics 指标
+     * @return 结果
+     */
+    int updateMetrics(MetricsDO metrics);
+
+    /**
+     * 删除指标
+     *
+     * @param id 指标主键
+     * @return 结果
+     */
+    int deleteMetricsById(Long id);
+
+    /**
+     * 批量删除指标
+     *
+     * @param ids 需要删除的数据主键集合
+     * @return 结果
+     */
+    int deleteMetricsByIds(Long[] ids);
+}
