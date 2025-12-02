36c6ef6a4dcbd05bcd9888c3421e46ecc2bd900c
sosumecho <sosumecho@gmail.com>
2025-12-02 22:48:21 +0800

    修改接口

--- RuoYi-Vue3-master/src/api/business/asset.js
+++ RuoYi-Vue3-master/src/api/business/asset.js
@@ -1,61 +1,60 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 // 查询资产列表
 export function listAsset(query) {
   return request({
-    url: '/asset/list',
-    method: 'get',
-    params: query
-  })
+    url: "/asset/list",
+    method: "get",
+    params: query,
+  });
 }
 
 // 查询资产详细
 export function getAsset(id) {
   return request({
-    url: '/asset/' + id,
-    method: 'get'
-  })
+    url: "/asset/" + id,
+    method: "get",
+  });
 }
 
 // 新增资产
 export function addAsset(data) {
   return request({
-    url: '/asset',
-    method: 'post',
-    data: data
-  })
+    url: "/asset",
+    method: "post",
+    data: data,
+  });
 }
 
 // 修改资产
 export function updateAsset(data) {
   return request({
-    url: '/asset',
-    method: 'put',
-    data: data
-  })
+    url: "/asset",
+    method: "put",
+    data: data,
+  });
 }
 
 // 删除资产
 export function delAsset(id) {
   return request({
-    url: '/asset/' + id,
-    method: 'delete'
-  })
+    url: "/asset/" + id,
+    method: "delete",
+  });
 }
 
 // 批量修改资产
 export function updateAssetBatch(data) {
   return request({
-    url: '/asset/batch',
-    method: 'put',
-    data: data
-  })
+    url: "/asset/batch",
+    method: "put",
+    data: data,
+  });
 }
 
 export function disassociateAsset(ids) {
   return request({
-    url: '/asset/disassociate/ids',
-    method: 'put',
-    data: ids
-  })
+    url: `/asset/disassociate/${ids}`,
+    method: "put",
+  });
 }
