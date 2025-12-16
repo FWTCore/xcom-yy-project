b41e4a5751a6414a2db466dc3de38e7e69bae624
xuchase <121086507@qq.com>
2025-12-16 08:25:24 +0800

    添加redis锁的bean

--- RuoYi-Vue-master/ruoyi-framework/src/main/java/com/ruoyi/framework/config/RedisConfig.java
+++ RuoYi-Vue-master/ruoyi-framework/src/main/java/com/ruoyi/framework/config/RedisConfig.java
@@ -8,6 +8,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.integration.redis.util.RedisLockRegistry;
 
 /**
  * redis配置
@@ -16,12 +17,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  */
 @Configuration
 @EnableCaching
-public class RedisConfig extends CachingConfigurerSupport
-{
+public class RedisConfig extends CachingConfigurerSupport {
     @Bean
     @SuppressWarnings(value = { "unchecked", "rawtypes" })
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory)
-    {
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<Object, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
@@ -40,8 +39,7 @@ public class RedisConfig extends CachingConfigurerSupport
     }
 
     @Bean
-    public DefaultRedisScript<Long> limitScript()
-    {
+    public DefaultRedisScript<Long> limitScript() {
         DefaultRedisScript<Long> redisScript = new DefaultRedisScript<>();
         redisScript.setScriptText(limitScriptText());
         redisScript.setResultType(Long.class);
@@ -51,19 +49,18 @@ public class RedisConfig extends CachingConfigurerSupport
     /**
      * 限流脚本
      */
-    private String limitScriptText()
-    {
-        return "local key = KEYS[1]\n" +
-                "local count = tonumber(ARGV[1])\n" +
-                "local time = tonumber(ARGV[2])\n" +
-                "local current = redis.call('get', key);\n" +
-                "if current and tonumber(current) > count then\n" +
-                "    return tonumber(current);\n" +
-                "end\n" +
-                "current = redis.call('incr', key)\n" +
-                "if tonumber(current) == 1 then\n" +
-                "    redis.call('expire', key, time)\n" +
-                "end\n" +
-                "return tonumber(current);";
+    private String limitScriptText() {
+        return "local key = KEYS[1]\n" + "local count = tonumber(ARGV[1])\n" + "local time = tonumber(ARGV[2])\n"
+               + "local current = redis.call('get', key);\n" + "if current and tonumber(current) > count then\n"
+               + "    return tonumber(current);\n" + "end\n" + "current = redis.call('incr', key)\n"
+               + "if tonumber(current) == 1 then\n" + "    redis.call('expire', key, time)\n" + "end\n"
+               + "return tonumber(current);";
+    }
+
+    @Bean
+    public RedisLockRegistry redisLockRegistry(RedisConnectionFactory redisConnectionFactory) {
+        // 第一个参数：Redis连接工厂
+        // 第二个参数：锁的注册表键（registryKey），通常用于区分不同的应用
+        return new RedisLockRegistry(redisConnectionFactory, "xcom-yy-lock", 300000L);
     }
 }
